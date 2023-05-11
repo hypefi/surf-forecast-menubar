be999cac46608312cb0fed5bf9c384decc1694b3
Anas Boukharta <anasboukharta@gmail.com>
2023-05-11 11:37:40 +0100

    browser

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { Menu, Tray, app, BrowserWindow } = require('electron')
+let tray = null
+let win = null 
 
 function createWindow () {
   let win = new BrowserWindow({
@@ -12,4 +14,45 @@ function createWindow () {
   win.loadFile('index.html')
 }
 
-app.whenReady().then(createWindow)
+// app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+tray = new Tray('./assets/icons/icons8-wave-22.png') // Provide the path to your tray icon here
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Open', click: function() { win.show() } },
+    { label: 'Quit', role: 'quit' }
+  ])
+  tray.setToolTip('My Electron Tray App')
+  tray.setContextMenu(contextMenu)
+
+  win = new BrowserWindow({
+    width: 600,
+    height: 450,
+    show: false,
+frame: false, // change to true to be able to drag 
+    fullscreenable: false,
+    resizable: true,
+    transparent: false,
+    webPreferences: {
+      backgroundThrottling: false
+    }
+  })
+  win.loadFile('index.html') // Load your application here
+
+  win.on('blur', () => {
+    if (!win.webContents.isDevToolsOpened()) {
+      win.hide()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
