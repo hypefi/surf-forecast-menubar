8cceba4e487513696167c346284e621f6c0901f1
Anas Boukharta <anasboukharta@gmail.com>
2023-05-14 16:17:32 +0100

    chart - 3 - wip

--- app/view/index.js
+++ app/view/index.js
@@ -19,54 +19,77 @@ const labels = Array.from({length: 7}, (_, i) => moment().month(i).format('MMMM'
 
 // const moment = require('chartjs-adapter-moment');
 
+const fs = require('fs');
 
-let data = [
-    {
-        // "timestamp": 1683846000,
-        "timestamp": 1,
-        "probability": 100,
-        "utcOffset": 1,
-        "surf_min": 1.5,
-        "surf_max": 2.1,
-        "surf_optimalScore": 2,
-        "surf_plus": false,
-        "surf_humanRelation": "Head to 0.6m overhead",
-        "surf_raw_min": 1.35825,
-        "surf_raw_max": 2.12227,
-    },
-    {
-        // "timestamp": 1683849600,
-        "timestamp": 2,
-        "probability": 100,
-        "utcOffset": 1,
-        "surf_min": 1.5,
-        "surf_max": 2.4,
-        "surf_optimalScore": 2,
-        "surf_plus": false,
-        "surf_humanRelation": "Head to well overhead",
-        "surf_raw_min": 1.36819,
-        "surf_raw_max": 2.1378,
-    }
-    // ... more data
-];
+// Read the JSON file
+
+let data;
+
+fs.readFile('/Users/admin/Desktop/WORK/CODE/2023/surf-forecast/pyth-surf/waves.json', 'utf8', (err, jsondata) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(jsondata)
+  data = jsondata; 
+  // Parse the JSON data into a JavaScript object
+  // const jsonData = JSON.parse(data);
+
+  // Copy the contents to a variable
+  // const data = jsonData;
+
+  // Use the copiedData variable as needed
+  // console.log(data);
+});
+
+// let data = [
+//     {
+//         "timestamp": 1683846000,
+//         // "timestamp": 1,
+//         "probability": 100,
+//         "utcOffset": 1,
+//         "surf_min": 1.5,
+//         "surf_max": 2.1,
+//         "surf_optimalScore": 2,
+//         "surf_plus": false,
+//         "surf_humanRelation": "Head to 0.6m overhead",
+//         "surf_raw_min": 1.35825,
+//         "surf_raw_max": 2.12227,
+//     },
+//     {
+//         "timestamp": 1683849600,
+//         // "timestamp": 2,
+//         "probability": 100,
+//         "utcOffset": 1,
+//         "surf_min": 1.5,
+//         "surf_max": 2.4,
+//         "surf_optimalScore": 2,
+//         "surf_plus": false,
+//         "surf_humanRelation": "Head to well overhead",
+//         "surf_raw_min": 1.36819,
+//         "surf_raw_max": 2.1378,
+//     }
+//     // ... more data
+// ];
 
 // const labels = Utils.months({count: 7});
 
-const data2 = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
-};
+// const data2 = {
+//   labels: labels,
+//   datasets: [{
+//     label: 'My First Dataset',
+//     data: [65, 59, 80, 81, 56, 55, 40],
+//     fill: false,
+//     borderColor: 'rgb(75, 192, 192)',
+//     tension: 0.1
+//   }]
+// };
 
-const config = {
-  type: 'line',
-  data: data2,
-};
+// const config = {
+//   type: 'line',
+//   data: data2,
+// };
 
 // Preparing your data for the chart
 let timestamps = data.map(obj => new Date(obj.timestamp * 1000));
@@ -76,56 +99,43 @@ let surf_max = data.map(obj => obj.surf_max);
 
 
 document.addEventListener('DOMContentLoaded', () => {
-const Chart = require('chart.js/auto').Chart;
-    // const LinearScale = require('chart.js').scales.Linear;
-    // const PointElement = require('chart.js').elements.Point;
-    // const Tooltip = require('chart.js').plugins.tooltip;
-    // const Legend = require('chart.js').plugins.legend;
-    // const TimeScale = require('chart.js').scales.Time;
-
-    // Chart.register(TimeScale);
-    //     Chart.controllers.Line.controller, // or whatever type of chart you're creating
-    //     Chart.elements.Point,
-    //     Chart.scales.Linear,
-    //     Chart.scales.Time,
-    //     Chart.plugins.Legend,
-    //     Chart.plugins.Tooltip
-    // );
+    const Chart = require('chart.js/auto').Chart;
 
     let ctx = document.getElementById('myChart').getContext('2d');
 
-    let myChart = new Chart(ctx, config);
-    // let myChart = new Chart(ctx, {
-    //     type: 'line',
-    //     data: {
-    //         labels: timestamps,
-    //         datasets: [{
-    //             label: 'surf_min',
-    //             data: surf_min,
-    //             backgroundColor: 'rgba(75, 192, 192, 0.2)',
-    //             borderColor: 'rgba(75, 192, 192, 1)',
-    //             borderWidth: 1
-    //         },
-    //         {
-    //             label: 'surf_max',
-    //             data: surf_max,
-    //             backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    //             borderColor: 'rgba(255, 99, 132, 1)',
-    //             borderWidth: 1
-    //         }]
-    //     },
-    //     options: {
-    //         scales: {
-    //             x: {
-    //                 display: true,
-    //             },
-    //             y: {
-    //                 display: true,
-    //                 beginAtZero: true
-    //             }
-    //         }
-    //     }
-    // });
+    // let myChart = new Chart(ctx, config);
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timestamps,
+            datasets: [{
+                label: 'surf_min',
+                data: surf_min,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'surf_max',
+                data: surf_max,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        // options: {
+        //     scales: {
+        //         x: {
+        //             type: 'time',
+        //             display: true,
+        //         },
+        //         y: {
+        //             display: true,
+        //             beginAtZero: true
+        //         }
+        //     }
+        // }
+    });
 });
 
 // Creating the chart
